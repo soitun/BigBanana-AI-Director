79fa242ac1c08fd8effb7bb637f78a4f7f3075a9
zyxucp <286513187@qq.com>
2026-03-15 21:49:11 +0800

    Remove model type definitions and Vite configuration files

--- components/account-center/internal.ts
+++
@@ -1,3 +0,0 @@
-export type AuthTab = 'login' | 'register';
-
-export type { LoginFormState, RegisterFormState, TokenFormState } from './types';
--- constants/links.ts
+++
@@ -1,8 +0,0 @@
-export const USER_MANUAL_URL = 'https://ocnf8yod3ljg.feishu.cn/wiki/TLjKwROQ0iIyg8kjjDhcU1TlnVg';
-
-export const OFFICIAL_WEBSITE_URL = 'https://tree456.com/';
-export const CREATIVE_HOME_URL = 'https://bigbanana.tree456.com/';
-export const DIRECTOR_HUB_URL = 'https://directorhub.tree456.com/';
-export const APP_VERSION = '1.0.0';
-export const COPYRIGHT_TEXT = `© ${new Date().getFullYear()} 武汉树语智能科技有限公司`;
-export const COMPANY_NAME = '武汉树语智能科技有限公司';
